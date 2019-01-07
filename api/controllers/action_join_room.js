3e4c8f81ff9f9066b3ca44e32e733ff671d81ca6
Léo Le Diouron <leo.lediouron.pro@gmail.com>
2019-01-07 19:52:08 +0100

    error handling for menu + return to menu button

--- api/controllers/action_join_room.js
+++ api/controllers/action_join_room.js
@@ -5,14 +5,18 @@ function action_join_room(req, res, infos_games) {
     var room_id = req.query.room_id;
     var player_id = createId.create_player_id();
 
-    if (infos_games[room_id].addPlayer(new Player(player_id)) == true) {
-        res.status(200).json({
-            'room_id': room_id,
-            'player_id': player_id,
-            'cash': infos_games[room_id].getPlayer(player_id).getCash()
-        });
+    if (room_id in infos_games) {
+        if (infos_games[room_id].addPlayer(new Player(player_id)) == true) {
+            res.status(200).json({
+                'room_id': room_id,
+                'player_id': player_id,
+                'cash': infos_games[room_id].getPlayer(player_id).getCash()
+            });
+        } else {
+            res.status(500).json({'error_message':'not enough place'});
+        }
     } else {
-        res.status(200).json({});
+        res.status(500).json({'error_message':'this room doesn\'t exist'});
     }
     return infos_games;
 }
--- api/controllers/action_random_room.js
+++ api/controllers/action_random_room.js
@@ -12,10 +12,10 @@ function action_random_room(req, res, infos_games, room_id) {
                 'cash': infos_games[room_id].getPlayer(player_id).getCash()
             });
         } else {
-            res.status(200).json({});
+            res.status(500).json({'error_message':'not enough place'});
         }
     } else {
-        res.status(500).json({});
+        res.status(500).json({'error_message':'no room avaible'});
     }
     return infos_games;
 }
